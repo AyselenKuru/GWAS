33362c1e1670364b1c09df9cf89ac3344e09f0ce
AyselenKuru <93535485+AyselenKuru@users.noreply.github.com>
2023-08-06 20:02:52 +0200

    Update GWAS.py

--- GWAS.py
+++ GWAS.py
@@ -1,6 +1,16 @@
 import requests
 import json
+def extract_rsIds(json_data):
+    rsIds_list = []
 
+    if "_embedded" in json_data and "singleNucleotidePolymorphisms" in json_data["_embedded"]:
+        snps_data = json_data["_embedded"]["singleNucleotidePolymorphisms"]
+
+        for snp in snps_data:
+            if "rsId" in snp:
+                rsIds_list.append(snp["rsId"])
+
+    return rsIds_list
 def query_gwas_by_gene(gene_name):
     api_url = f"https://www.ebi.ac.uk/gwas/rest/api/singleNucleotidePolymorphisms/search/findByGene"
     params = {
@@ -16,6 +26,11 @@ def query_gwas_by_gene(gene_name):
         # Save the SNP data to a JSON file
         with open("GWAS.json", "w") as json_file:
             json.dump(snp_data, json_file, indent=4)
+        with open('GWAS.json', 'r') as f:
+            json_data = json.load(f)
+        list= extract_rsIds(json_data)
+        for rs in list:
+            print(rs)
 
         print(f"GWAS data for gene {gene_name} retrieved and saved as GWAS.json")
 
@@ -26,5 +41,6 @@ def query_gwas_by_gene(gene_name):
 
 # Usage example
 if __name__ == "__main__":
-    gene_name = "SLC30A8"
+    #gene_name = "SLC30A8"
+    gene_name = input('Enter the gene name: ')
     query_gwas_by_gene(gene_name)
